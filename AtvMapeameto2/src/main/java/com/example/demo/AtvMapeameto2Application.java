275984f50e8137665fbbd964f1b72015ac66dbdd
Gisele03 <132359841+Gisele03@users.noreply.github.com>
2024-03-14 13:56:58 -0300

    Add files via upload

---
+++ AtvMapeameto2/src/main/java/com/example/demo/AtvMapeameto2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvMapeameto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvMapeameto2Application.class, args);
+	}
+
+}
